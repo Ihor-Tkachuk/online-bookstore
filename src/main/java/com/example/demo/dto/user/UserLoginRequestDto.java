f5e4040b032b88d67d9c09b008c83e04927fc4e4
Ihor Tkachuk <iono19031@gmail.com>
2025-02-07 21:46:49 +0200

    fixed comments

--- src/main/java/com/example/demo/dto/user/UserLoginRequestDto.java
+++ src/main/java/com/example/demo/dto/user/UserLoginRequestDto.java
@@ -2,7 +2,6 @@ package com.example.demo.dto.user;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 public record UserLoginRequestDto(
@@ -10,7 +9,7 @@ public record UserLoginRequestDto(
         @Length(min = 8, max = 20)
         @Email
         String email,
-        @NotEmpty
+        @NotBlank
         @Length(min = 6, max = 20)
         String password
 ) {
--- src/main/java/com/example/demo/repository/user/UserRepository.java
+++ src/main/java/com/example/demo/repository/user/UserRepository.java
@@ -3,8 +3,6 @@ package com.example.demo.repository.user;
 import com.example.demo.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends
-        JpaRepository<User, Long> {
-
+public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
 }
