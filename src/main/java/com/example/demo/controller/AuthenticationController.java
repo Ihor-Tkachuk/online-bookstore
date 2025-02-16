653aac3113c97b1977670be1f38cfde4ac0cb733
Ihor Tkachuk <iono19031@gmail.com>
2025-02-16 21:20:24 +0200

    added solution

--- src/main/java/com/example/demo/controller/AuthenticationController.java
+++ src/main/java/com/example/demo/controller/AuthenticationController.java
@@ -1,8 +1,11 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.user.UserLoginRequestDto;
+import com.example.demo.dto.user.UserLoginResponseDto;
 import com.example.demo.dto.user.UserRegistrationRequestDto;
 import com.example.demo.dto.user.UserResponseDto;
 import com.example.demo.exception.RegistrationException;
+import com.example.demo.security.AuthenticationService;
 import com.example.demo.service.user.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -19,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 public class AuthenticationController {
     private final UserService userService;
+    private final AuthenticationService authenticationService;
 
     @Operation(summary = "User registration",
             description = "Initial registration of new user")
@@ -26,5 +30,12 @@ public class AuthenticationController {
     public UserResponseDto register(@RequestBody @Valid UserRegistrationRequestDto requestDto)
             throws RegistrationException {
         return userService.register(requestDto);
+    }
+
+    @Operation(summary = "User authentication",
+            description = "Authentication of registered users")
+    @PostMapping("/login")
+    public UserLoginResponseDto login(@RequestBody @Valid UserLoginRequestDto requestDto) {
+        return authenticationService.authenticate(requestDto);
     }
 }
